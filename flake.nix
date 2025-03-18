7b4c15a06202c83a7f875bc3ec05914f3824c406
permalik <permalik@protonmail.com>
2025-03-17 21:57:17 -0500

    feat: init config

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "permalik flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          ./configuration.nix
+	  home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.permalik = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ save.sh
@@ -0,0 +1,7 @@
+#!/run/current-system/sw/bin/bash
+sudo cp /etc/nixos/.gitignore .
+sudo cp /etc/nixos/configuration.nix .
+sudo cp /etc/nixos/flake.lock .
+sudo cp /etc/nixos/flake.nix .
+sudo cp /etc/nixos/hardware-configuration.nix .
+sudo cp /etc/nixos/home.nix .
