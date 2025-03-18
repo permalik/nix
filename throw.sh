9ee4386902ed59f77dcd431e158d7cf88e8a0946
permalik <permalik@protonmail.com>
2025-03-17 22:05:19 -0500

    feat: impl catch and throw scripts

---
+++ throw.sh
@@ -0,0 +1,7 @@
+#!/run/current-system/sw/bin/bash
+sudo cp ./.gitignore /etc/nixos
+sudo cp ./configuration.nix /etc/nixos
+sudo cp ./flake.lock /etc/nixos
+sudo cp ./flake.nix /etc/nixos
+sudo cp ./hardware-configuration.nix /etc/nixos
+sudo cp ./home.nix /etc/nixos
