45680112b6c73c656e1c9f7e5232ea436a901b55
permalik <permalik@protonmail.com>
2025-03-22 16:22:17 -0500

    feat: modularize config

--- flake.nix
+++ flake.nix
@@ -2,28 +2,69 @@
   description = "permalik flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+# Nixpkgs
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
 
+# Home Manager
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        modules = [
-          ./configuration.nix
-	  home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.permalik = import ./home.nix;
-          }
-        ];
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+  inherit (self) outputs;
+
+  users = {
+    permalik = {
+      name = "permalik";
     };
   };
+
+  initNixosConfiguration = hostname: username:
+    nixpkgs.lib.nixosSystem {
+      modules = [./hosts/${hostname}];
+    };
+
+  initHomeConfiguration = system: username: hostname:
+    home-manager.lib.homeManagerConfiguration {
+      pkgs = import nixpkgs {inherit system;};
+      modules = [
+        ./configuration.nix
+        ./home/${username}/${hostname}
+      ];
+    };
+
+  in {
+    nixosConfigurations = {
+      linux = initNixosConfiguration "linux" "permalik";
+    };
+
+    homeConfigurations = {
+      "permalik@linux" = initHomeConfiguration "aarch64-linux" "permalik" "linux";
+    };
+
+    overlays = import ./overlays {inherit inputs;};
+  };
+
+# nixosConfigurations = {
+#   nixos = nixpkgs.lib.nixosSystem {
+#     system = "aarch64-linux";
+#     modules = [
+#       ./configuration.nix
+#         home-manager.nixosModules.home-manager
+#         {
+#           home-manager.useGlobalPkgs = true;
+#           home-manager.useUserPackages = true;
+#           home-manager.users.permalik = import ./home.nix;
+#         }
+#     ];
+#   };
+# };
 }
--- home.nix
+++ home.nix
@@ -30,71 +30,8 @@
 	};
 
 	programs.neovim = {
-		plugins = [
-			## Language Utilities
-			pkgs.vimPlugins.nvim-treesitter
-			pkgs.vimPlugins.nvim-treesitter.withAllGrammars
-			pkgs.vimPlugins.nvim-treesitter-textobjects
-			pkgs.vimPlugins.nvim-lspconfig
-			pkgs.vimPlugins.clangd_extensions-nvim
-			pkgs.vimPlugins.gitsigns-nvim
-				
-			## Treesitter
-			pkgs.vimPlugins.telescope-nvim
-			pkgs.vimPlugins.telescope-fzf-native-nvim
-
-			## Tooling
-			pkgs.vimPlugins.trouble-nvim
-			pkgs.vimPlugins.plenary-nvim
-			pkgs.vimPlugins.lualine-nvim
-			pkgs.vimPlugins.fidget-nvim
-			pkgs.vimPlugins.harpoon2
-
-			## CMP
-			pkgs.vimPlugins.nvim-cmp
-			pkgs.vimPlugins.cmp-nvim-lsp
-			pkgs.vimPlugins.cmp-buffer
-			pkgs.vimPlugins.cmp-cmdline
-			pkgs.vimPlugins.luasnip
-			pkgs.vimPlugins.cmp_luasnip
-
-			## Debug
-			pkgs.vimPlugins.nvim-dap
-			pkgs.vimPlugins.nvim-dap-ui
-			pkgs.vimPlugins.nvim-dap-virtual-text
-
-			## TPope
-			pkgs.vimPlugins.vim-surround
-			pkgs.vimPlugins.vim-obsession
-			pkgs.vimPlugins.vim-sleuth
-
-			## Theme
-			pkgs.vimPlugins.kanagawa-nvim
-		];
-
-		extraConfig = ''
-			lua << EOF
-			${builtins.readFile config/keymap.lua}
-			${builtins.readFile config/option.lua}
-			${builtins.readFile config/setup/cmp.lua}
-			${builtins.readFile config/setup/treesitter.lua}
-			${builtins.readFile config/setup/lspconfig.lua}
-			${builtins.readFile config/setup/luasnip.lua}
-			${builtins.readFile config/setup/trouble.lua}
-			${builtins.readFile config/setup/telescope.lua}
-			${builtins.readFile config/setup/lualine.lua}
-			${builtins.readFile config/setup/fidget.lua}
-			${builtins.readFile config/setup/gitsigns.lua}
-			${builtins.readFile config/setup/harpoon.lua}
-			${builtins.readFile config/setup/clangd_extensions.lua}
-			${builtins.readFile config/setup/dap.lua}
-		'';
 		enable = true;
-		viAlias = true;
-		vimAlias = true;
 	};
-
-	xdg.configFile."nvim/init.lua".enable = false;
 
 	home.stateVersion = "24.11";
 
