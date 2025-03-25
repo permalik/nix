75563c2d16dad9b430882fe9b71ed2bc665b1ee3
permalik <permalik@protonmail.com>
2025-03-24 23:48:54 -0500

    feat: revert config

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
 	description = "permalik nixos + nix-darwin";
 
 	inputs = {
-		# Nixos
+	# Nixos
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-		
-		# Home Manager
+
+	# Home Manager
 		home-manager = {
 			url = "github:nix-community/home-manager/release-24.11";
 			inputs.nixpkgs.follows = "nixpkgs";
@@ -17,51 +17,7 @@
 		nixpkgs,
 		home-manager,
 		...
-	} @ inputs: let
-		inherit (self) outputs;
-		
-		users = {
-			permalik = {
-				name = "permalik";
-			};
-		};
-
-		initNixosConfiguration = hostname: username:
-			nixpkgs.lib.nixosSystem {
-				specialArgs = {
-					inherit inputs outputs hostname;
-					userConfig = users.${username};
-					nixosModules = "${self}/modules/nixos";
-				};
-				modules = [./hosts/${hostname}];
-			};
-
-		initHomeConfiguration = system: username: hostname:
-			home-manager.lib.homeManagerConfiguration {
-				pkgs = import nixpkgs {inherit system;};
-				extraSpecialArgs = {
-					inherit inputs outputs;
-					userConfig = users.${username};
-					permalikModules = "${self}/modules/home-manager";
-				};
-				modules = [
-					./home/${username}/${hostname}
-				];
-			};
-
-		in {
-			nixosConfigurations = {
-				linux = initNixosConfiguration "linux" "permalik";
-			};
-
-			homeConfigurations = {
-				"permalik@linux" = initHomeConfiguration "x86_64-linux" "permalik" "linux";
-			};
-			
-			overlays = import ./overlays {inherit inputs;};
-		};
-		
-		/*
+	} @ inputs: {
 		nixosConfigurations = {
 			nixos = nixpkgs.lib.nixosSystem {
 				system = "x86_64-linux";
@@ -77,5 +33,5 @@
 				];
 			};
 		};
-		*/
+	};
 }
--- rebuild.sh
+++ rebuild.sh
@@ -7,28 +7,18 @@ sudo mv /etc/nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.
 sudo mv /etc/nixos/home.nix /etc/nixos/home.nix.bak
 
 # Migrate Configuration
+sudo cp /home/permalik/Docs/Git/nix/configuration.nix /etc/nixos
 sudo cp /home/permalik/Docs/Git/nix/flake.lock /etc/nixos
 sudo cp /home/permalik/Docs/Git/nix/flake.nix /etc/nixos
-sudo cp -r /home/permalik/Docs/Git/nix/home /etc/nixos
-sudo cp -r /home/permalik/Docs/Git/nix/hosts /etc/nixos
-sudo cp -r /home/permalik/Docs/Git/nix/modules /etc/nixos
-sudo cp -r /home/permalik/Docs/Git/nix/overlays /etc/nixos
+sudo cp /home/permalik/Docs/Git/nix/hardware-configuration.nix /etc/nixos
+sudo cp /home/permalik/Docs/Git/nix/home.nix /etc/nixos
 
 # Rebuild Environment
 cd /etc/nixos
-sudo nixos-rebuild switch --flake /etc/nixos#linux
+sudo nixos-rebuild switch
 
 if [ $? -eq 0 ]; then
 	echo "Success: NixOS rebuild complete."
-else
-	echo "Error: NixOS rebuild failed."
-	exit 1
-fi
-
-home-manager switch --flake /etc/nixos#permalik@linux
-
-if [ $? -eq 0 ]; then
-	echo "Success: home-manager rebuild complete."
 	# Remove Backups
 	sudo rm /etc/nixos/configuration.nix.bak
 	sudo rm /etc/nixos/flake.lock.bak
@@ -37,7 +27,7 @@ if [ $? -eq 0 ]; then
 	sudo rm /etc/nixos/home.nix.bak
 	echo "Backups removed."
 else
-	echo "Error: home-manager rebuild failed."
+	echo "Error: NixOS rebuild failed."
 	exit 1
 fi
 
