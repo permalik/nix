14432d847c2346f4a57156089eeb2f7c781a0f81
permalik <permalik@protonmail.com>
2025-05-13 00:02:10 -0500

    feat: configure lualine

--- home-manager/programs/nixcats/lua/plugins/lualine.lua
+++ home-manager/programs/nixcats/lua/plugins/lualine.lua
@@ -1,13 +1,6 @@
-return {
-    "nvim-lualine/lualine.nvim",
-    event = "VimEnter",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require("lualine").setup {
-            options = {
-                theme = "ayu_mirage",
-                -- theme = "nord",
-            },
-        }
-    end,
+require("lualine").setup {
+    options = {
+        theme = "ayu_mirage",
+        -- theme = "nord",
+    },
 }
