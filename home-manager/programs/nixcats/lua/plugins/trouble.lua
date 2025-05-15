ec5da5e07364a22a4ddec90e2a39f34cffeed471
permalik <permalik@protonmail.com>
2025-05-15 00:37:13 -0500

    feat: update null-ls, trouble config

--- home-manager/programs/nixcats/lua/plugins/trouble.lua
+++ home-manager/programs/nixcats/lua/plugins/trouble.lua
@@ -1,36 +1,38 @@
-require("trouble").setup {
-    opts = {},
-    cmd = "Trouble",
-    keys = {
-        {
-            "<leader>xx",
-            "<cmd>Trouble diagnostics toggle<cr>",
-            desc = "diagnostics (trouble)",
-        },
-        {
-            "<leader>xX",
-            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-            desc = "buffer diagnostics (trouble)",
-        },
-        {
-            "<leader>cs",
-            "<cmd>Trouble symbols toggle focus=false<cr>",
-            desc = "symbols (trouble)",
-        },
-        {
-            "<leader>cl",
-            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-            desc = "lsp definitions / references / ... (trouble)",
-        },
-        {
-            "<leader>xL",
-            "<cmd>Trouble loclist toggle<cr>",
-            desc = "location list (trouble)",
-        },
-        {
-            "<leader>xQ",
-            "<cmd>Trouble qflist toggle<cr>",
-            desc = "quickfix list (trouble)",
-        },
-    },
-}
+vim.keymap.set(
+    "n",
+    "<leader>xx",
+    "<cmd>Trouble diagnostics toggle<cr>",
+    { desc = "diagnostics (trouble)" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>xX",
+    "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+    { desc = "buffer diagnostics (trouble)" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>cs",
+    "<cmd>Trouble symbols toggle focus=false<cr>",
+    { desc = "symbols (trouble)" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>cl",
+    "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+    { desc = "lsp definitions / references / ... (trouble)" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>xL",
+    "<cmd>Trouble loclist toggle<cr>",
+    { desc = "location list (trouble)" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>xQ",
+    "<cmd>Trouble qflist toggle<cr>",
+    { desc = "quickfix list (trouble)" }
+)
+
+require("trouble").setup {}
