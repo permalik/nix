c0a4f3148bfe6b9942178c2bed0003026c430b3b
permalik <permalik@protonmail.com>
2025-07-04 17:39:46 -0500

    feat: update tmux conf

--- home-manager/programs/tmux/default.nix
+++ home-manager/programs/tmux/default.nix
@@ -34,3 +34,31 @@
     '';
   };
 }
+# set -g mouse on
+# unbind C-b
+# set -g prefix C-a
+# bind C-Space send-prefix
+#
+# setw -g mode-keys vi
+# bind-key h select-pane -L
+# bind-key j select-pane -D
+# bind-key k select-pane -U
+# bind-key l select-pane -R
+# bind '"' split-window -c "#{pane_current_path}"
+# bind '%' split-window -h -c "#{pane_current_path}"
+# bind-key v split-window -v -c "#{pane_current_path}" -l '25%'
+# bind-key b split-window -h -c "#{pane_current_path}" -l '30%'
+#
+# set-option -g terminal-overrides 'xterm-256color:RGB'
+# set -g @plugin 'tmux-plugins/tpm'
+# set -g @plugin 'tmux-plugins/tmux-resurrect'
+# set -g @resurrect-strategy-nvim 'session'
+# set -g status-left-length 85
+# set -g status-left "tmalik#[fg=colour135] "
+# set -g window-status-current-format "#[fg=black,bold bg=default]│#[fg=white bg=cyan]#W#[fg=black,bold bg=default]│"
+# set -g window-status-current-format "#[fg=black,bold bg=default]│#[fg=colour135 bg=black]#W#[fg=black,bold bg=default]│"
+# set -g status-style bg=default
+# set -g status-right "#[fg=black] #[bg=red] %b %d %Y %l:%M %p"
+#
+# run '~/.tmux/plugins/tpm/tpm'
+
