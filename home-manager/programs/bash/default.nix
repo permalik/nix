6a2cd5d6efb109a01d6c6795a91a74d60644fed6
permalik <permalik@protonmail.com>
2026-05-05 08:46:08 -0500

    feat:: nixos: add aliases

--- home-manager/programs/bash/default.nix
+++ home-manager/programs/bash/default.nix
@@ -10,9 +10,11 @@
   programs.bash = {
     enable = true;
     initExtra = ''
+      source "$HOME/.config/bash/alias.sh"
       source "$HOME/.config/bash/git-log.sh"
     '';
   };
 
+  xdg.configFile."./bash/alias.sh".source = ./alias.sh;
   xdg.configFile."./bash/git-log.sh".source = ./git-log.sh;
 }
