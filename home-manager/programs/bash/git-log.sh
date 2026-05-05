eaa724fc9aa2713b83658868d38593298ce665cd
permalik <permalik@protonmail.com>
2026-05-05 08:04:35 -0500

    fix:: nixos: pretty git log formatting

--- home-manager/programs/bash/git-log.sh
+++ home-manager/programs/bash/git-log.sh
@@ -3,7 +3,7 @@ alias gl="pretty_git_log"
 pretty_git_log() {
     local hash relative_time author refs subject format
 
-    hash="C(always,yellow)%h%C(always,reset)"
+    hash="%C(always,yellow)%h%C(always,reset)"
     relative_time="%C(always,green)%ar%C(always,reset)"
     author="%C(always,bold blue)%an%C(always,reset)"
     refs="%C(always,red)%d%C(always,reset)"
