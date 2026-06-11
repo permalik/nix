2009d93dc090ac39b1fab1bdb2e2f7fda988f50b
permalik <permalik@protonmail.com>
2026-06-10 22:43:44 -0500

    feat: configure zls

--- home-manager/programs/nvim/lua/plugins/lsp.lua
+++ home-manager/programs/nvim/lua/plugins/lsp.lua
@@ -211,6 +211,31 @@ vim.lsp.config.rust_analyzer = {
     },
 }
 
+vim.lsp.config["zls"] = {
+    -- Set to 'zls' if `zls` is in your PATH
+    cmd = { "/Users/tymalik/.local/bin/zls/zls" },
+    filetypes = { "zig" },
+    root_markers = { "build.zig" },
+    -- There are two ways to set config options:
+    --   - edit your `zls.json` that applies to any editor that uses ZLS
+    --   - set in-editor config options with the `settings` field below.
+    --
+    -- Further information on how to configure ZLS:
+    -- https://zigtools.org/zls/configure/
+    settings = {
+        zls = {
+            -- Whether to enable build-on-save diagnostics
+            --
+            -- Further information about build-on save:
+            -- https://zigtools.org/zls/guides/build-on-save/
+            -- enable_build_on_save = true,
+
+            -- omit the following line if `zig` is in your PATH
+            -- zig_exe_path = "/path/to/zig_executable",
+        },
+    },
+}
+
 -- Baseline config
 -- vim.lsp.config.rust_analyzer = {
 --   settings = {
@@ -248,7 +273,6 @@ local enable = {
     "ts_ls",
     "vimls",
     "yamlls",
-    "zls",
 
     -- explicitly configured
     "ccls",
@@ -258,6 +282,7 @@ local enable = {
     "cssls",
     "racket_langserver",
     "rust_analyzer",
+    "zls",
 }
 
 for name, config in pairs(servers) do
